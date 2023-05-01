b4b6d71dae76dc804cc1332a38bb1b02069d33ec
KevinB19 <112514118+KevinB19@users.noreply.github.com>
2023-04-30 20:21:16 -0400

    Update KevinBretthauer- A4Q2.js

--- KevinBretthauer- A4Q2.js
+++ KevinBretthauer- A4Q2.js
@@ -1,71 +1,89 @@
+// Set the number of rows and columns for the grid
+let rows = 3;
+let columns = 5;
+
 function setup() {
-  createCanvas(960, 540);
-
+  createCanvas(400, 400);
   angleMode(DEGREES);
-  noLoop();
-  tR = 3;
-  tC = 5;
-  tW = width / tR;
-  tH = height / tC;
 
+  // Calculate the width and height of each cell in the grid
+  widths = width / columns;
+  heights = height / rows;
+
+  // Stop the draw loop from running automatically
+  noLoop();
+
+  // Define three stars with different properties
   star1 = {
-    x: 50,
-    y: 50,
-    scale: 0.5,
+    startx: 50,
+    starty: 65,
+    scale: 1,
+    points: 5,
+    startAngle: 20,
+    fill: "orange",
+  };
+  star2 = {
+    startx: 25,
+    starty: 85,
+    scale: 1.5,
     points: 8,
     startAngle: 20,
-    fill: "teal",
+    fill: "purple",
   };
-
-  star2 = {
-    x: 20,
-    y: 20,
-    scale: 0.25,
-    points: 5,
-    startAngle: 69,
-    fill: "blue",
-  };
-
   star3 = {
-    x: 120,
-    y: 40,
-    scale: 0.75,
-    points: 18,
-    startAngle: 60,
-    fill: "orange",
+    startx: 30,
+    starty: 35,
+    scale: 2,
+    points: 10,
+    startAngle: 20,
+    fill: "teal",
   };
 }
 
 function draw() {
   background(220);
-  for (let i = 0; i < tC; i++) {
-    for (let j = 0; j < tR; j++) {
-      let oR = i * tW
-      let oL = j * tH
-      star1.x = star1.x + oR;
-      star2.x = star2.x + oR;
-      star3.x = star3.x + oR;
-      star1.y = star1.y + oL;
-      star2.y = star2.y + oL;
-      star3.y = star3.y + oL;
-      addStar(star1);
-      addStar(star2);
+
+  // Loop through each cell in the grid and draw stars in each cell
+  for (let rowsDrawn = 0; rowsDrawn < rows; rowsDrawn++) {
+    let offsetDown = rowsDrawn * heights;
+
+    star1.y = star1.starty + offsetDown;
+    star2.y = star2.starty + offsetDown;
+    star3.y = star3.starty + offsetDown;
+    for (let columnsDrawn = 0; columnsDrawn < columns; columnsDrawn++) {
+      let offsetRight = columnsDrawn * widths;
+      star1.x = star1.startx + offsetRight;
+      star2.x = star2.startx + offsetRight;
+      star3.x = star3.startx + offsetRight;
+
+      // Add stars to the current cell of the grid
       addStar(star3);
+      addStar(star2);
+      addStar(star1);
     }
   }
 }
 
 function addStar(star) {
   push();
-  translate(star.x, star.y);
-  scale(star.scale);
-  rotate(star.startAngle);
   fill(star.fill);
-  noStroke();
-  let addAngle = 360 / star.points;
-  for (let i = 0; i < star.points; i++) {
-    rotate(addAngle);
-    triangle(0, 50, 20, 0, -20, 0);
+
+  // Translate to the center of the star's position and apply a rotation
+  translate(star.x, star.y);
+  rotate(star.startAngle);
+
+  // Draw a rectangle and scale it
+  rect(0, 0, 10);
+  scale(star.scale);
+
+  // Calculate the angle between each point on the star
+  let addangle = 360 / star.points;
+  for (let linesDrawn = 0; linesDrawn < star.points; linesDrawn++) {
+    // Rotate and draw each point of the star
+    rotate(addangle);
+    noStroke();
+    triangle(0, -20, 10, 0, -10, 0);
   }
   pop();
 }
+
